7288bb171a0df95df8e247a80619f2d15bd65cbd
Michael Brock <michael.brock@dfaw.com.au>
2021-06-18 14:13:15 +1000

    Removed old auth-wrapper as it is now done in a context.
    However, the example from the SkillBakery tutorial is kept for later reference if need be.

--- fe-web-react/src/contexts/AuthorizationContext/index.jsx
+++ fe-web-react/src/contexts/AuthorizationContext/index.jsx
@@ -1,3 +1,31 @@
+// Old version taken from SkillBakery.  Retained a second attempt is made with that approach.
+/*import authentication from '@kdpw/msal-b2c-react';
+import decodeJWT from 'jwt-decode';
+
+class AuthWrapper {
+    isLoggedIn() {
+        const token = this.getToken();
+
+        return( token ? true : false );
+    }
+
+    logout() {
+        authentication.signOut();
+    }
+
+    getToken() {
+        return authentication.getAccessToken();
+    }
+
+    currentUser() {
+        const decoded = decodeJWT(this.getToken());
+
+        return( {
+            email: decoded.emails[ 0 ],
+        } );
+    }
+}*/
+
 // Sourced from: https://dev.to/finiam/predictable-react-authentication-with-the-context-api-g10
 
 import {
