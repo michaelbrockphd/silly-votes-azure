5df9bb89344ce1cf83a7d685dce1fbc845852010
Michael Brock <michael.brock@dfaw.com.au>
2021-06-24 20:47:03 +1000

    Committing current progress towards a dispatcher.

--- fe-web-react/src/containers/UserCampaignsContainer/index.jsx
+++ fe-web-react/src/containers/UserCampaignsContainer/index.jsx
@@ -1,26 +1,69 @@
-import { Fragment, useEffect, useState } from 'react';
+import {
+    Fragment,
+    useEffect,
+    useReducer } from 'react';
 import { useAuthorization } from '../../contexts/AuthorizationContext';
 import CampaignTable, { CampaignTableLoading } from '../../components/CampaignTable';
 
 import WebApi from '../../web-api';
 
+const ACTION_ADD_CAMPAIGN = "ADD_CAMPAIGN";
+const ACTION_CHANGE_CAMPAIGNS = "CHANGE_CAMPAIGNS";
+const ACTION_CHANGE_LOADING = "CHANGE_LOADING";
+const ACTION_REMOVE_CAMPAIGN = "REMOVE_CAMPAIGN";
+
+const reduceCampaignRemoval = (campaigns, target) => {
+    // TODO: Update the WebApi class to actually call the microservice.
+    const rtn = campaigns.filter( c => c._id !== target.id );
+
+    return( rtn );
+};
+
+const userCampaignReducer = (state, action) => {
+    switch(action.type) {
+        case ACTION_CHANGE_LOADING:
+            return { ...state, isLoading: action.value };
+
+        case ACTION_CHANGE_CAMPAIGNS:
+            return { ...state, campaigns: action.value };
+
+        case ACTION_REMOVE_CAMPAIGN:
+            return { ...state, campaigns: reduceCampaignRemoval(state.campaigns, action.value) };
+
+        default:
+            throw new Error( `${action.type} is not a recognised action.` );
+    }
+};
+
+const initialState = {
+    isLoading: TextTrackCueList,
+    campaigns: []
+};
+
 const UserCampaignsContainer = (props) => {
-    const [isLoading, setIsLoading] = useState( true );
-    const [campaigns, setCampaigns] = useState( [] );
+    const [{ isLoading, campaigns }, dispatch] = useReducer(userCampaignReducer, initialState);
 
     const { getToken } = useAuthorization();
 
-    useEffect(() => {
-        WebApi.getUserCampaigns( getToken() )
-              .then((response) => {
-                    setIsLoading( false );
+    const token = getToken();
 
-                    setCampaigns( response.data );
+    useEffect(() => {
+        WebApi.getUserCampaigns( token )
+              .then((response) => {
+                    dispatch({
+                        type: ACTION_CHANGE_CAMPAIGNS,
+                        value: response.data
+                    });
+
+                    dispatch({
+                        type: ACTION_CHANGE_LOADING,
+                        value: false
+                    });
               })
               .catch((err) => {
                   console.log(err);
               });
-    });
+    }, []);
 
     const addCampaign = () => {
         alert( "add campaign" );
